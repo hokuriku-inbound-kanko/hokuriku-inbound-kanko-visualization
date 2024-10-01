9621460a47d93d327c66eeb8d598c6b207affbca
yamaji <yamaji@jig.jp>
2024-10-01 15:56:08 +0900

    👍 国籍と年代のグラフのコンポーネントを追加

---
+++ components/graphs/age-range-graph.component.tsx
@@ -0,0 +1,44 @@
+import { DataService } from "@/service/data.service";
+import { DateService } from "@/service/date.service";
+import Papa from "papaparse";
+import Card from "../card.component";
+import { Graph } from "../graph.component";
+
+export default async function AgeRangeGraph() {
+  const dataService = new DataService();
+
+  const data = Papa.parse<string[]>(
+    await dataService.getSpan("hokuriku-gift-campaign", DateService.nDaysAgo(8)),
+  );
+
+  const answers: { answer: string; from: number; to: number; count: number }[] = [];
+  for (let i = 0; i < 80; i += 10) {
+    answers.push({ answer: `${i}歳~${i + 9}歳`, from: i, to: i + 9, count: 0 });
+  }
+  answers.push({ answer: "80歳~", from: 80, to: Infinity, count: 0 });
+  data.data
+    .map((row) => new Date().getFullYear() - Number(row[8]))
+    .forEach((answer) => {
+      answers.forEach((v, i) => {
+        if (answer >= v.from && answer <= v.to) {
+          answers[i].count++;
+        }
+      });
+    });
+
+  const options = {
+    series: [
+      {
+        data: answers.map((v) => ({ x: v.answer, y: v.count })),
+      },
+    ],
+  };
+
+  console.log(options);
+
+  return (
+    <Card title="年代">
+      <Graph type="bar" series={options.series} options={options} />
+    </Card>
+  );
+}
