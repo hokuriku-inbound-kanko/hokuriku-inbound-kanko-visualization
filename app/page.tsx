942324a2fbfc1b9c016d76ba71175c514a4d7d02
yamaji <yamaji@jig.jp>
2024-10-01 17:09:40 +0900

    👍 訪問目的のグラフを作成して表示

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import GenderGraph from "@/components/graphs/gender-graph.component";
 import HowManyVisitsJapanGraph from "@/components/graphs/how-many-visits-japan-graph.component";
 import InfoSourceGraph from "@/components/graphs/info-source-graph.component";
 import NationalitiesGraph from "@/components/graphs/nationalities-graph.component";
+import PurposeOfVisitFacilityGraph from "@/components/graphs/purpose-of-visit-facility-graph.component";
+import PurposeOfVisitPrefectureGraph from "@/components/graphs/purpose-of-visit-prefecture-graph.component";
+import PurposeOfVisitRegionGraph from "@/components/graphs/purpose-of-visit-region-graph.component";
 
 export default async function Home() {
   return (
@@ -14,6 +17,9 @@ export default async function Home() {
         <GenderGraph />
         <InfoSourceGraph />
         <HowManyVisitsJapanGraph />
+        <PurposeOfVisitRegionGraph />
+        <PurposeOfVisitPrefectureGraph />
+        <PurposeOfVisitFacilityGraph />
       </div>
     </>
   );
---
+++ components/graphs/purpose-of-visit-prefecture-graph.component.tsx
@@ -0,0 +1,48 @@
+import { DataService } from "@/service/data.service";
+import { DateService } from "@/service/date.service";
+import Card from "../card.component";
+import { Graph } from "../graph.component";
+import Papa from "papaparse";
+
+export default async function PurposeOfVisitPrefectureGraph() {
+  const dataService = new DataService();
+
+  const data = Papa.parse<string[]>(
+    await dataService.getSpan("hokuriku-gift-campaign", DateService.nDaysAgo(8)),
+  );
+
+  const answers = data.data
+    .map((row) => row[66])
+    .filter((v) => !!v)
+    .map((v) =>
+      v
+        .toString()
+        .trim()
+        .split("\n")
+        .map((v) => v.trim()),
+    )
+    .flat()
+    .reduce(
+      (p: { answer: string; count: number }[][], c) => {
+        if (!p[0].map((v) => v.answer).includes(c)) p[0].push({ answer: c, count: 0 });
+        p[0][p[0].map((v) => v.answer).indexOf(c)].count += 1;
+        return p;
+      },
+      [[]],
+    )[0]
+    .sort((a, b) => a.count - b.count);
+
+  const options = {
+    series: [
+      {
+        data: answers.map((v) => ({ x: v.answer, y: v.count })),
+      },
+    ],
+  };
+
+  return (
+    <Card title="県を訪問した目的（複数回答あり）">
+      <Graph type="bar" series={options.series} options={options} />
+    </Card>
+  );
+}
