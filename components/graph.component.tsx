855c56ce58c8b9eb7735043ea48fd853f9ff3e00
yamaji <yamaji@jig.jp>
2024-10-01 17:53:52 +0900

    👍 window.innerWidthをuseEffectの中でのみ参照するように

--- components/graph.component.tsx
+++ components/graph.component.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import dynamic from "next/dynamic";
+import { useEffect, useState } from "react";
 import type { Props } from "react-apexcharts";
 const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
 
@@ -9,13 +10,17 @@ const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
  * @param props Apex ChartのChartコンポーネントにわたすプロパティ
  */
 export function Graph(props: Required<Pick<Props, "type" | "series" | "options">>) {
+  const [innerWidth, setInnerWidth] = useState(640);
+  useEffect(() => {
+    setInnerWidth(window.innerWidth);
+  });
   return (
     <Chart
       className="grid h-fit w-[280px] place-content-center sm:h-[270px] sm:w-[360px]"
       type={props?.type}
       series={props?.series}
-      height={window.innerWidth <= 640 ? 210 : 270}
-      width={window.innerWidth <= 640 ? 280 : 360}
+      height={innerWidth <= 640 ? 210 : 270}
+      width={innerWidth <= 640 ? 280 : 360}
       options={props?.options}
     />
   );
