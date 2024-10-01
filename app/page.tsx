e283fa93c09ab96e8a5b9fd1f7c22360607fb409
yamaji <yamaji@jig.jp>
2024-10-01 15:56:32 +0900

    👍 追加したグラフを表示してレスポンシブに対応

--- app/page.tsx
+++ app/page.tsx
@@ -1,55 +1,14 @@
-import Card from "@/components/card.component";
-import { Graph } from "@/components/graph.component";
-import { DataService } from "@/service/data.service";
-import Papa from "papaparse";
+import AgeRangeGraph from "@/components/graphs/age-range-graph.component";
+import NationalitiesGraph from "@/components/graphs/nationalities-graph.component";
 
 export default async function Home() {
-  const dataService = new DataService();
-
-  const data = Papa.parse<string[]>(
-    await dataService.getSpan(
-      "hokuriku-gift-campaign",
-      new Date(Date.now() - 1000 * 60 * 60 * 24 * 8),
-    ),
-  );
-
-  const answeredAt = data.data
-    .map((row) => row[10])
-    .filter((v) => !!v)
-    .map((v) => v.toString().trim())
-    .reduce(
-      (p: { answer: string; count: number }[][], c) => {
-        if (!p[0].map((v) => v.answer).includes(c)) p[0].push({ answer: c, count: 0 });
-        p[0][p[0].map((v) => v.answer).indexOf(c)].count += 1;
-        return p;
-      },
-      [[]],
-    )[0]
-    .sort((a, b) => b.count - a.count);
-
-  const options = {
-    series: answeredAt.map((v) => v.count),
-    labels: answeredAt.map((v) => v.answer),
-    plotOptions: {
-      pie: {
-        donut: {
-          labels: {
-            show: true,
-            total: {
-              show: true,
-            },
-          },
-        },
-      },
-    },
-  };
-
   return (
     <>
       <h2 className="mb-4 text-xl font-bold">直近1週間のアンケート結果</h2>
-      <Card title="国籍">
-        <Graph type="donut" series={options.series} options={options} width={""} height={""} />
-      </Card>
+      <div className="grid h-full w-full grid-cols-1 gap-4 sm:grid-cols-2 xl:grid-cols-3">
+        <NationalitiesGraph />
+        <AgeRangeGraph />
+      </div>
     </>
   );
 }
--- components/graph.component.tsx
+++ components/graph.component.tsx
@@ -8,13 +8,14 @@ const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
  * クライアントでApexChartのグラフを表示するコンポーネント
  * @param props Apex ChartのChartコンポーネントにわたすプロパティ
  */
-export function Graph(props: Required<Props>) {
+export function Graph(props: Required<Pick<Props, "type" | "series" | "options">>) {
   return (
     <Chart
+      className="grid h-fit w-[280px] place-content-center sm:h-[270px] sm:w-[360px]"
       type={props?.type}
       series={props?.series}
-      height={props?.height}
-      width={props?.width}
+      height={window.innerWidth <= 640 ? 210 : 270}
+      width={window.innerWidth <= 640 ? 280 : 360}
       options={props?.options}
     />
   );
