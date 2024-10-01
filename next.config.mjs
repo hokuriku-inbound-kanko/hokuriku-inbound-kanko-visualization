9790e5be39ac8468985933afc09b9b0127ad8db2
yamaji <yamaji@jig.jp>
2024-10-01 22:43:39 +0900

    🚑 BasePathを指定してPagesでのリンクが壊れないように

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: process.env.PAGES ? "/hokuriku-inbound-kanko-visualization" : "",
 };
 
 export default nextConfig;
