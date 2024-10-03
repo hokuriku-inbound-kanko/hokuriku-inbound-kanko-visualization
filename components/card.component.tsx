e29e9a2db7c040c564fa8a7392b8ca215c2457fe
yamaji <yamaji@jig.jp>
2024-10-03 13:03:27 +0900

    👍 CLSを防ぐ+グラフのローディング表示を追加

--- components/card.component.tsx
+++ components/card.component.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 export default function Card(props: Props) {
   return (
-    <article className="flex flex-col items-center gap-y-4 rounded-lg bg-separator p-4">
+    <article className="flex h-[calc(210px+1em+2em+54px)] flex-col items-center gap-y-4 rounded-lg bg-separator p-4 sm:h-[calc(270px+1em+2em+54px)]">
       <h3 className="text-lg font-bold">{props.title}</h3>
       {props.children}
     </article>
--- components/graph.component.tsx
+++ components/graph.component.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { GraphIcon } from "@primer/octicons-react";
 import dynamic from "next/dynamic";
 import { useEffect, useState } from "react";
 import type { Props } from "react-apexcharts";
@@ -15,13 +16,18 @@ export function Graph(props: Required<Pick<Props, "type" | "series" | "options">
     setInnerWidth(window.innerWidth);
   });
   return (
-    <Chart
-      className="grid h-fit w-[280px] place-content-center sm:h-[270px] sm:w-[360px]"
-      type={props?.type}
-      series={props?.series}
-      height={innerWidth <= 640 ? 210 : 270}
-      width={innerWidth <= 640 ? 280 : 360}
-      options={{ ...props?.options, chart: { toolbar: { show: false } } }}
-    />
+    <div className="relative grid h-fit w-[280px] place-content-center sm:h-[270px] sm:w-[360px]">
+      <div className="absolute left-0 top-0 grid h-fit w-[280px] place-content-center sm:h-[270px] sm:w-[360px]">
+        <GraphIcon className="animate-pulse fill-surface" size="large" />
+      </div>
+      <Chart
+        className="absolute left-0 top-0 grid h-fit w-[280px] place-content-center bg-separator sm:h-[270px] sm:w-[360px]"
+        type={props?.type}
+        series={props?.series}
+        height={innerWidth <= 640 ? 210 : 270}
+        width={innerWidth <= 640 ? 280 : 360}
+        options={{ ...props?.options, chart: { toolbar: { show: false } } }}
+      />
+    </div>
   );
 }
