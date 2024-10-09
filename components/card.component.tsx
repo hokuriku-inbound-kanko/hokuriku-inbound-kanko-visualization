93f9e2a21c25557af23604373bc0c0c0dcb82a42
yamaji <yamaji@jig.jp>
2024-10-09 13:34:38 +0900

    👍 カードコンポーネントにclassNameを指定できるように修正

--- components/card.component.tsx
+++ components/card.component.tsx
@@ -3,11 +3,14 @@ import { ReactNode } from "react";
 type Props = {
   title: string;
   children: ReactNode;
+  className: string;
 };
 
 export default function Card(props: Props) {
   return (
-    <article className="flex h-[calc(210px+1em+2em+54px)] flex-col items-center gap-y-4 rounded-lg bg-separator p-4 sm:h-[calc(270px+1em+2em+54px)]">
+    <article
+      className={`flex h-[calc(210px+1em+2em+54px)] flex-col items-center gap-y-4 rounded-lg bg-separator p-4 sm:h-[calc(270px+1em+2em+54px)] ${props.className}`}
+    >
       <h3 className="text-lg font-bold">{props.title}</h3>
       {props.children}
     </article>
