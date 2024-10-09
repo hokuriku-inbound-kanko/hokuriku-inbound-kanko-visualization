688062c13794c8b3ecb3140420a801c0f1d7bb6c
yamaji <yamaji@jig.jp>
2024-10-09 18:05:10 +0900

    🚑 tfootの内容がtrなしで書かれてしまっていたのを修正

--- components/table.component.tsx
+++ components/table.component.tsx
@@ -170,12 +170,14 @@ export function Table(props: TableProps) {
           ))}
         </tbody>
         <tfoot className="font-bold">
-          <th className="border-r-2 border-t-2 border-primary px-4 py-2" scope="row">
-            行数
-          </th>
-          <td className="border-r-2 border-t-2 border-primary px-4 py-2 text-right">
-            {tableBody.length}
-          </td>
+          <tr>
+            <th className="border-r-2 border-t-2 border-primary px-4 py-2" scope="row">
+              行数
+            </th>
+            <td className="border-r-2 border-t-2 border-primary px-4 py-2 text-right">
+              {tableBody.length}
+            </td>
+          </tr>
         </tfoot>
       </table>
     </div>
