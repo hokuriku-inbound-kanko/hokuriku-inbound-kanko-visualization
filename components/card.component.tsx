9eadd326ad668af486d746a7eaabb4db363a2587
yamaji <yamaji@jig.jp>
2024-10-09 14:13:03 +0900

    🚑 CardコンポーネントのclassNameをOptionalに修正

--- components/card.component.tsx
+++ components/card.component.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 type Props = {
   title: string;
   children: ReactNode;
-  className: string;
+  className?: string;
 };
 
 export default function Card(props: Props) {
